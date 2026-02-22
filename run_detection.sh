e1e3f107599ad7a47bc324131c9008d8e30caca8
fengming001ntu <fengming001@e.ntu.edu.sg>
2026-02-22 16:12:15 +0800

    Update and rename run_detection.sh to run_eval.sh

--- run_detection.sh
+++
@@ -1,6 +0,0 @@
-python3 detection.py \
-  0 \
-  seed \
-  ../eval_csv/eval30.csv \
-  ../z_ckpt/exp_dsr/step=02400.lora_only.ckpt_eval_30 \
-  1 0
---
+++ run_eval.sh
@@ -0,0 +1,13 @@
+python3 detection.py \
+  0 \
+  seed \
+  ../eval_csv/eval30.csv \
+  ../z_ckpt/exp_dsr/step=02400.lora_only.ckpt_eval_30 \
+  1 0
+
+python3 calc_dsr_score.py \
+  ./eval_csv/eval30.csv \
+  ../z_ckpt/exp_dsr/step=02400.lora_only.ckpt_eval_30_det
+
+python3 calc_correctness.py \
+  ../z_ckpt/exp_dsr/result_DSRscoreV5_step=02400.lora_only.ckpt_eval_30_det.csv
